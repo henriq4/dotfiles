87636f494865c1a1862678d00462ae57a9954c52
Henrique GC <henriq4net@gmail.com>
2023-02-19 12:54:59 -0300

    chore: adding new keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
 require "core.settings"
 require "core.keymappings"
+
 require "plugins"
+require "plugins.configs.lua-line"
+require "plugins.configs.autoclose"
+require "plugins.configs.nvim-tree"
+require "plugins.configs.wilder"
+require "plugins.configs.treesitter"
+require "plugins.theme.tokyodark"
+require "plugins.lsp"
+
